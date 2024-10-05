75851d59c68e4b767666265012576757d368080d
dhomini <dhominibp4@gmail.com>
2024-10-05 13:06:48 -0300

    First commit with base cc file

---
+++ redes.cc
@@ -0,0 +1,50 @@
+/*
+ * Universidade Federal do Pará
+ * Instituto de Tecnologia - ITEC
+ * Faculdade de Compuutação e Telecomunicações
+ *
+ * Contrucao de Topologia de Redes Simples Utilizando ns-3
+ * Redes de Computadores - 2024.2
+ *
+ * Dhomini Bezerra Picanco - 202306840009
+ * Antonio Carlos Bessa Martins Neto - 202306840039
+ * Rafael Felipe Pantoja Sales - 202306840003
+ */
+
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/network-module.h"
+#include "ns3/point-to-point-module.h"
+
+using namespace ns3;
+
+int
+main(int argc, char *argv[])
+{
+	Time::SetResolution(Time::NS);
+
+	NodeContainer nodes;
+	nodes.Create(10);
+
+	PointToPointHelper pointToPoint;
+	pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+	pointToPoint.SetChannelAttribute("Delay", StringValue("5ms"));
+
+	NetDeviceContainer devices;
+	devices = pointToPoint.Install(nodes);
+
+	InternetStackHelper stack;
+	stack.Install(nodes);
+
+	Ipv4AddressHelper address;
+	address.SetBase("10.1.1.0", "255.255.255.0");
+
+	Ipv4InterfaceContainer interfaces = address.Assign(devices);
+
+
+	Simulator::Run();
+	Simulator::Destroy();
+	
+	return 0;
+}
